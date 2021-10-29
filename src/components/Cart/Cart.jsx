1faed22612594b2c39b5476eef55ba21277e32a0
r-ILIASS <92882494+r-ILIASS@users.noreply.github.com>
2021-10-29 17:51:06 +0100

    Fix:fix cart rendering error

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 
 import useStyles from "./styles";
+import Cartitem from "./Cartitem/Cartitem";
 
 const Cart = ({ cart }) => {
-  const isEmpty = !(cart.line_items && cart.line_items.length);
   const classes = useStyles();
 
   const EmptyCart = () => (
@@ -18,7 +18,7 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <Cartitem item={item} />
           </Grid>
         ))}
       </Grid>
@@ -49,6 +49,7 @@ const Cart = ({ cart }) => {
       </div>
     </>
   );
+  if (!cart.line_items) return "loading";
 
   return (
     <Container>
@@ -56,7 +57,7 @@ const Cart = ({ cart }) => {
       <Typography className={classes.title} variant="h3">
         Your Shopping Cart
       </Typography>
-      {isEmpty ? <EmptyCart /> : <FilledCart />}
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   );
 };
---
+++ src/components/Cart/Cartitem/Cartitem.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const Cartitem = ({ item }) => {
+  const classes = useStyles();
+  console.log(item);
+
+  return (
+    <Card>
+      <CardMedia image={item.image.url} alt="alt" className={classes.media} />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button type="button" size="small">
+            -
+          </Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type="button" size="small">
+            +
+          </Button>
+        </div>
+        <Button variant="contained" type="button" color="secondary">
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Cartitem;
